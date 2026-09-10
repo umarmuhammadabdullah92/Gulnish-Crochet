0e3be7097be043f09e4814bf4757e511da280411
Gulnish Crochet <gulnishcrochet@gmail.com>
2026-09-10 10:11:17 +0500

    Auto-deploy: 2026-09-10T05:11:17.890Z

--- js/script.js
+++ js/script.js
@@ -1059,9 +1059,25 @@
     }
   });
 
+  /* ---------- Contact buttons: WhatsApp + Call (from settings) ---------- */
+  function updateContactButtons() {
+    var waNum = GC && GC.shopWhatsApp ? GC.shopWhatsApp() : "";
+    var telHref = waNum ? "tel:+" + waNum : "tel:+923075729901";
+    var waHref = waNum
+      ? "https://wa.me/" + waNum + "?text=" + encodeURIComponent("Hi Gulnish Crochet, I'd like to place an order.")
+      : "";
+    document.querySelectorAll(".fb-wa").forEach(function (a) {
+      if (waHref) a.href = waHref;
+    });
+    document.querySelectorAll(".fb-call").forEach(function (a) {
+      a.href = telHref;
+    });
+  }
+
   /* ---------- Render shop once shared data is loaded ---------- */
   function renderShop() {
     var settings = getSettings();
+    updateContactButtons();
     buildFilters(settings);
     renderCategoryCards();
     renderFeatured();
