ef59ca5d401b64f9797748c724b60bc00e7c5fed
Gulnish Crochet <gulnishcrochet@gmail.com>
2026-09-24 00:26:30 +0500

    Auto-deploy: 2026-09-23T19:26:29.701Z

--- js/supabase.js
+++ js/supabase.js
@@ -81,7 +81,7 @@
 
   var REAL_PRODUCTS = {
     "seed_gr4_1": { name: "Bag 1", price: 1599 },
-    "seed_gr1_1": { name: "Crochet Rose Purse", price: 5799 },
+    "seed_gr1_1": { name: "Crochet Rose Purse", price: 4999 },
     "seed_gr1_2": { name: "Purse 2", price: 5799 },
     "seed_gr1_3": { name: "Purse 3", price: 5799 },
     "seed_gr1_10": { name: "Purse 10", price: 5799 },
