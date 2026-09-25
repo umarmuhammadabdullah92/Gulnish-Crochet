4ede7aed875cf2b89c328b206850390677f3ccf7
Gulnish Crochet <gulnishcrochet@gmail.com>
2026-09-25 22:04:35 +0500

    Auto-deploy: 2026-09-25T17:04:35.233Z

--- js/checkout.js
+++ js/checkout.js
@@ -127,8 +127,6 @@
     if (!items.length) {
       if (itemsEl) itemsEl.innerHTML = "";
       if (subtotalEl) subtotalEl.textContent = money(0);
-      var emptyShip = document.getElementById("coShipping");
-      if (emptyShip) emptyShip.hidden = true;
       if (emptyWrap) emptyWrap.hidden = false;
       if (formWrap) formWrap.hidden = true;
       setBarVisible(false);
